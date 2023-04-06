5d8da2b52463c44294bc52ad74b440ebe808ba2e
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-04-06 19:59:55 +0300

    fetchOffers refactored

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -2,14 +2,9 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import { ApiRoutes } from '@utils/const';
 import { loadOffers } from './action';
-import { InitialState } from '@customTypes/store';
 import { Offer } from '@customTypes/index';
-import { AppDispatch } from '@customTypes/store';
-
 
 export const fetchOffers = createAsyncThunk<void, undefined, {
-  dispatch: AppDispatch;
-  state: InitialState;
   extra: AxiosInstance;
 }>(
   'hotels/',
@@ -18,16 +13,3 @@ export const fetchOffers = createAsyncThunk<void, undefined, {
     dispatch(loadOffers(data));
   },
 );
-
-export const fetchOffer = createAsyncThunk<void, undefined, {
-  dispatch: AppDispatch;
-  state: InitialState;
-  extra: AxiosInstance;
-}>(
-  'hotels/:id',
-  async (_arg, {dispatch, extra: api}) => {
-    const {data} = await api.get<Offer[]>(ApiRoutes.offers);
-    dispatch(loadOffers(data));
-  },
-);
-
