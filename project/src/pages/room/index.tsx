5f95502785f425940b2ba92e0b5e9fad6ee82828
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-04-07 14:06:07 +0300

    fixes for clear 7.1 task

--- project/src/pages/room/index.tsx
+++ project/src/pages/room/index.tsx
@@ -6,7 +6,7 @@ import Suggestions from './components/suggestions';
 import Overview from './components/overview';
 import ReviewList from './components/reviewsList';
 import Map from '@components/map';
-import { findOfferById, findOfferNearby, findOfferReviews } from 'store/action';
+import { findOfferById } from 'store/action';
 import { InitialState } from '@customTypes/store';
 
 function Room() {
@@ -17,12 +17,10 @@ function Room() {
 
   useEffect(() => {
     dispatch(findOfferById(offerId));
-    dispatch(findOfferNearby(offerId));
-    dispatch(findOfferReviews(offerId));
   });
 
   if (offerToDisplay === undefined) {
-    return <Navigate to="not-found"/>;
+    return <Navigate to="/not-found"/>;
   }
 
   return (
